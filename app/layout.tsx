bbe10901ec08c53319b8c979475f43be6db1df74
gonzalolsk <gonzalolsk@gmail.com>
2024-08-21 18:33:36 -0300

    delete unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./ui/header/Header";
-import logo from "@/public/images/logo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
