1455bceeb6d5aa34dde37a1138e677a827d4c56d
gonzalolsk <gonzalolsk@gmail.com>
2024-09-01 03:16:52 +0000

    add new component ShowScrollProgress

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import SectionWelcome from './ui/sectionWelcome/SectionWelcome';
 import SectionAbout from './ui/sectionAbout/sectionAbout';
 import SectionExpertise from './ui/sectionExpertise/SectionExpertise';
 import Contact from './ui/contact/Contact';
+import ShowScrollProgress from './ui/showScrollProgress/ShowScrollProgress';
 
 export default function Home() {
   
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <>  
       <main className="flex flex-col justify-between bg-slate-200 dark:bg-[#2c1338] text-slate-800 dark:text-slate-50 " id="home">
+          <ShowScrollProgress />
           <SectionWelcome />
           <div className='diagonal-border bg-white dark:bg-slate-400'></div>
           <SectionAbout />
---
+++ app/ui/showScrollProgress/ShowScrollProgress.tsx
@@ -0,0 +1,29 @@
+'use client';
+ import { useState, useEffect } from 'react';
+
+const ShowScrollProgress: React.FC = () => {
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalHeight = document.body.scrollHeight - window.innerHeight;
+      const progress = (window.scrollY / totalHeight) * 100;
+      setScrollProgress(progress);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="fixed top-[80px] left-0 w-full h-1 bg-gray-200 z-50">
+      <div
+        className="h-full bg-blue-500"
+        style={{ width: `${scrollProgress}%` }}
+      />
+    </div>
+  );
+};
+
+export default ShowScrollProgress;
