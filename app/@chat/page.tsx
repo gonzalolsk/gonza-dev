e1e78b9c9117e7daa768dbad9f7a2a04dfdd7c0c
gonzalolsk <gonzalolsk@gmail.com>
2024-09-02 23:02:47 +0000

    update chat and scrollbars styles

--- app/@chat/page.tsx
+++ app/@chat/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { sendQuestion } from "./actions";
-
+import style from './chat.module.css'
 interface Message {
     id: string;
     text: string;
@@ -68,25 +68,28 @@ export default function ChatPage() {
         <>
             <button
                 id="chat-button"
-                className="fixed bottom-4 right-6 z-10 bg-slate-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-slate-600 focus:outline-none"
+                className={`fixed bottom-4 right-6 z-10 text-white px-4 py-2 rounded-lg shadow-lg  focus:outline-none
+                hover:bg-slate-800
+                bg-${isChatOpen ? 'slate-800' : 'slate-900'}
+                `}
                 onClick={() => { setIsChatOpen(!isChatOpen); }}
             >
-                Ask me anything!
-                <span className="absolute bottom-0 right-4 transform translate-y-full w-0 h-0 border-t-8 border-t-slate-500 border-l-8 border-l-transparent border-r-8 border-r-transparent"></span>
+                {isChatOpen ? 'Close' : 'Ask me anything!'}
+                <span className="absolute bottom-0 right-4 transform translate-y-full w-0 h-0 border-t-8 border-t-slate-900 border-l-8 border-l-transparent border-r-8 border-r-transparent"></span>
             </button>
 
             {isChatOpen && (
-                <section id="opened-chat" className="p-4 grid gap-4 rounded bg-slate-600 fixed bottom-20 right-6 bg-opacity-95">
-                    <article id="msg-box" className="grid gap-4 max-h-[320px] h-[320px] items-start overflow-y-auto">
+                <section id="opened-chat" className={`z-10 p-4 grid gap-4 rounded bg-slate-900 fixed bottom-16 right-6 bg-opacity-95 max-w-xl`}>
+                    <article id="msg-box" className={`${style.scrollbar} grid gap-4 max-h-[320px] h-[320px] items-start overflow-y-auto p-4`}>
                         {messages.map(message => (
-                            <div key={message.id} className={`p-4 rounded-3xl bg-slate-700 text-white w-fit ${message.owner === 'user' ? 'bg-slate-900 ml-auto rounded-br-none' : 'rounded-bl-none'}`}>
+                            <div key={message.id} className={`p-4 rounded-3xl bg-slate-700 text-white w-fit ${message.owner === 'user' ? 'bg-thirdColor ml-auto rounded-br-none' : 'rounded-bl-none'}`}>
                                 {message.text}
                             </div>
                         ))}
                     </article>
 
                     <form onSubmit={handleSubmit} className="flex">
-                        <input className="rounded-l-lg px-4 leading-[2.5rem] bg-white text-black flex-1" type="text" name="question" />
+                        <input className="rounded-l-lg px-4 leading-[2.5rem] bg-white text-black flex-1 focus:outline-none" type="text" name="question" />
                         <button disabled={isLoading} className="disabled:opacity-50 bg-slate-500 leading-[2.5rem] px-4 rounded-r-lg text-white" type="submit">
                             {isLoading ? 'Sending...' : 'Send'}
                         </button>
